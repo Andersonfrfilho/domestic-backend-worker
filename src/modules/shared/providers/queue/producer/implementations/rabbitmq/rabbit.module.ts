b652e6e597c6cc1e363eac4d06c76453e0d65cc2
Anderson <andersonfrfilho@gmail.com>
2026-04-30 13:52:00 -0300

    fix(worker): make WorkerRabbitMQModule @Global() to prevent duplicate RabbitMQ connections

--- src/modules/shared/providers/queue/producer/implementations/rabbitmq/rabbit.module.ts
+++ src/modules/shared/providers/queue/producer/implementations/rabbitmq/rabbit.module.ts
@@ -1,6 +1,5 @@
 import { LoggerModule } from '@adatechnology/logger';
 import { Module } from '@nestjs/common';
-import { WorkerRabbitMQModule } from '@modules/shared/rabbitmq/rabbitmq.module';
 
 import { QUEUE_PRODUCER_PROVIDER } from '../../producer.token';
 
@@ -8,7 +7,7 @@ import { RabbitBindingsService } from './rabbit.bindings.service';
 import { RabbitMQMessageProducer } from './rabbit.provider';
 
 @Module({
-  imports: [WorkerRabbitMQModule, LoggerModule],
+  imports: [LoggerModule],
   providers: [
     {
       provide: QUEUE_PRODUCER_PROVIDER,
