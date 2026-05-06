57ebbd452e5ffa8968cda2f47c9ec4d0798a2fbd
Anderson <andersonfrfilho@gmail.com>
2026-05-06 17:43:48 -0300

    fix(worker): make queue implementation module conditional
    When DISABLE_RABBITMQ=true, skip importing RabbitMQ module and use NoopMessageProducer instead of RabbitMQMessageProducer.

--- src/modules/shared/providers/queue/producer/implementations/queue.producer.implementation.module.ts
+++ src/modules/shared/providers/queue/producer/implementations/queue.producer.implementation.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { SharedInfrastructureProviderQueueProducerImplementationsRabbitMqModule } from './rabbitmq/rabbit.module';
+import { QUEUE_PRODUCER_PROVIDER } from '../producer.token';
+import { NoopMessageProducer } from './noop/noop.provider';
 
 @Module({
-  imports: [SharedInfrastructureProviderQueueProducerImplementationsRabbitMqModule],
-  exports: [SharedInfrastructureProviderQueueProducerImplementationsRabbitMqModule],
+  imports: process.env.DISABLE_RABBITMQ === 'true' ? [] : [SharedInfrastructureProviderQueueProducerImplementationsRabbitMqModule],
+  providers: process.env.DISABLE_RABBITMQ === 'true'
+    ? [{ provide: QUEUE_PRODUCER_PROVIDER, useClass: NoopMessageProducer }]
+    : [],
+  exports: process.env.DISABLE_RABBITMQ === 'true' ? [QUEUE_PRODUCER_PROVIDER] : [SharedInfrastructureProviderQueueProducerImplementationsRabbitMqModule],
 })
 export class SharedInfrastructureProviderQueueProducerImplementationsModule {}
