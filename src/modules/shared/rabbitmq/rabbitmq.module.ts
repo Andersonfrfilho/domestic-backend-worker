5f36573b6c0764ec31d198e1160a51e2ae5cf923
Anderson <andersonfrfilho@gmail.com>
2026-05-06 21:17:30 -0300

    fix(worker): revert RabbitMQ connectionInitOptions wait to false to avoid 5s timeout

--- src/modules/shared/rabbitmq/rabbitmq.module.ts
+++ src/modules/shared/rabbitmq/rabbitmq.module.ts
@@ -15,7 +15,7 @@ const logger = new Logger('RabbitMQModule');
 
         return {
           uri,
-          connectionInitOptions: { wait: true },
+          connectionInitOptions: { wait: false },
           connectionManagerOptions: { heartbeatIntervalInSeconds: 60, reconnectTimeInSeconds: 2 },
           prefetchCount: Number(process.env.RABBITMQ_PREFETCH ?? 10),
         };
